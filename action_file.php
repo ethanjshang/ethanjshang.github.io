c25d37202005ada85572bd1f89a2e3dc4a8ac307
Ethan Shang <34875955+ethanjshang@users.noreply.github.com>
2018-08-15 16:57:01 -0700

    PHP file for message sending

---
+++ action_file.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["Name"]; ?><br>
+Your email address is: <?php echo $_POST["Email"]; ?><br>
+This was your message: <?php echo$_POST["Message"; ?>
+
+</body>
+</html>
