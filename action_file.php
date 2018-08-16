677a827c4ebd156665a4e7f026fd3cf20328ab63
Ethan Shang <34875955+ethanjshang@users.noreply.github.com>
2018-08-15 17:02:11 -0700

    Delete action_file.php

--- action_file.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["Name"]; ?><br>
-Your email address is: <?php echo $_POST["Email"]; ?><br>
-This was your message: <?php echo$_POST["Message"; ?>
-
-</body>
-</html>
